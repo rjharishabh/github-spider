9864f8a5a6a392d34ca46a554c248b6f57cde6bf
Rishabh Ranjan Jha <61203226+rjharishabh@users.noreply.github.com>
2021-12-28 11:59:29 +0530

    add auth file

---
+++ auth.py
@@ -0,0 +1,16 @@
+'''
+This file is for the authentication process.
+In this file, please enter the github username and 
+personal access token to start the program.
+'''
+
+# Please enter the github username here
+user= 'github-username'
+
+# Please enter the personal access token
+token = 'personal-access-token'
+
+auth = (user, token)
+
+# User agent submitted to the Github
+header = {'user-agent': 'ghspider/1.0.0'}
